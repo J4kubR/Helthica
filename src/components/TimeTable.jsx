9e3828ae10f259a92464c4a23482765926e6dcc5
J4kub <platinium@aol.pl>
2023-11-08 13:57:41 +0100

    Made changes to the TimeTable file

--- src/components/TimeTable.jsx
+++ src/components/TimeTable.jsx
@@ -1,20 +1,81 @@
-const TimeTable = () => {
-  const currentDate = new Date();
+import format from "date-fns/format";
+import getDay from "date-fns/getDay";
+import parse from "date-fns/parse";
+import startOfWeek from "date-fns/startOfWeek";
+import React, { useState } from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { enUS } from "date-fns/locale";
+
+const locales = {
+  "en-US": enUS,
+};
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [];
+
+function TimeTable() {
+  const [newEvent, setNewEvent] = useState({ title: "", start: "", end: "" });
+  const [allEvents, setAllEvents] = useState(events);
+
+  function handleAddEvent() {
+    for (let i = 0; i < allEvents.length; i++) {
+      const d1 = new Date(allEvents[i].start);
+      const d2 = new Date(newEvent.start);
+      const d3 = new Date(allEvents[i].end);
+      const d4 = new Date(newEvent.end);
+
+      if ((d1 <= d2 && d2 <= d3) || (d1 <= d4 && d4 <= d3)) {
+        alert("CLASH");
+        break;
+      }
+    }
+
+    setAllEvents([...allEvents, newEvent]);
+  }
+
   return (
-    <div>
-      <div className="container rounded-5">
-        <button type="button" className="btn button btn-primary rounded-5">
-          Left arrow
-        </button>
-        <button type="button" className="btn button btn-primary rounded-5">
-          {currentDate.toDateString()}
-        </button>
-        <button type="button" className="btn button btn-primary rounded-5">
-          Right arrow
+    <div className="container rounded-5">
+      <div className="form-section">
+        <input
+          type="text"
+          placeholder="Add Title"
+          style={{ width: "20%", marginRight: "10px" }}
+          value={newEvent.title}
+          onChange={(e) => setNewEvent({ ...newEvent, title: e.target.value })}
+        />
+        <DatePicker
+          placeholderText="Start Date"
+          style={{ marginRight: "10px" }}
+          selected={newEvent.start}
+          onChange={(start) => setNewEvent({ ...newEvent, start })}
+        />
+        <DatePicker
+          placeholderText="End Date"
+          selected={newEvent.end}
+          onChange={(end) => setNewEvent({ ...newEvent, end })}
+        />
+        <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
+          Add Event
         </button>
       </div>
+      <Calendar
+        localizer={localizer}
+        events={allEvents}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500, margin: "50px" }}
+      />
     </div>
   );
-};
+}
 
 export default TimeTable;
---
+++ src/server.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+const json = bodyParser.json;
+
+
+// Connect to MongoDB
+mongoose.connect('mongodb+srv://J4kub:Xva49ucx@cluster0.dvzorzl.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Define a schema for your data
+const eventSchema = new mongoose.Schema({
+  date: String,
+  title: String,
+  description: String,
+});
+
+// Create a model from the schema
+const Event = mongoose.model('Event', eventSchema);
+
+const app = express();
+app.use(cors());
+app.use(json()); // For parsing application/json
+
+// API endpoint to get events
+app.get('/events', async (req, res) => {
+  try {
+    const events = await Event.find();
+    res.json(events);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// API endpoint to create a new event
+app.post('/events', async (req, res) => {
+  const event = new Event({
+    date: req.body.date,
+    title: req.body.title,
+    description: req.body.description,
+  });
+  try {
+    const newEvent = await event.save();
+    res.status(201).json(newEvent);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Start the server
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
