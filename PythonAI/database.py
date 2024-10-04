64481109e93f3ae5424115360a04150e51298e45
J4kub <platinium@aol.pl>
2024-10-04 21:24:42 +0200

    Database incorporated in the server.py

--- PythonAI/database.py
+++ PythonAI/database.py
@@ -12,15 +12,17 @@ cursor.execute(query)
 
 rows = cursor.fetchall()
 
-for row in rows:
-    name, address, age, last_doctor_visit, description, picture_url = row
-    print(f"Name: {name}")
-    print(f"Address: {address}")
-    print(f"Age: {age}")
-    print(f"Last Doctor Visit: {last_doctor_visit}")
-    print(f"Description: {description}")
-    print(f"Picture URL: {picture_url}")
-    print("-" * 40)
+print(len(rows))
+row = rows[9]
+
+id, name, address, age, last_doctor_visit, description, picture_url = row
+print(f"Name: {name}")
+print(f"Address: {address}")
+print(f"Age: {age}")
+print(f"Last Doctor Visit: {last_doctor_visit}")
+print(f"Description: {description}")
+print(f"Picture URL: {picture_url}")
+print("-" * 40)
 
 cursor.close()
 mysql_connection.close()
--- PythonAI/server.py
+++ PythonAI/server.py
@@ -2,11 +2,46 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from AIsolution import AI_prediction, tokenizer, maxlen
 from keras.models import load_model
+import mysql.connector
+
 
 app = Flask(__name__)
 CORS(app)
 
 model = load_model("./AI_pred_v1.h5")
+
+
+def find_database():
+    mysql_connection = mysql.connector.connect(
+        host="localhost", user="root", password="password", database="Patients"
+    )
+    cursor = mysql_connection.cursor()
+    query = "SELECT * from users"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    cursor.close()
+    mysql_connection.close()
+
+    return rows
+
+
+@app.route("/get-users", methods=["GET"])
+def get_database():
+    rows = find_database()
+    users = []
+    for row in rows:
+        id, name, address, age, last_doctor_visit, description, picture_url = row
+        user_data = {
+            "id": id,
+            "name": name,
+            "address": address,
+            "age": age,
+            "last_doctor_visit": last_doctor_visit,
+            "description": description,
+            "picture_url": picture_url,
+        }
+        users.append(user_data)
+    return jsonify(users)
 
 
 # getting the app to query a prediction then send it to the web server
